31ed52af15cd2b2f1841c71c9f24c6b427d95c4d
Rachit Dhamija <rachit.dhamija.96@gmail.com>
2021-05-30 03:30:02 -0700

    Added features for table 6, fixed url parsing and splitting

--- Url.py
+++ Url.py
@@ -1,4 +1,14 @@
 import requests
+from pprint import pprint
+from cymruwhois import Client
+import socket
+import whois
+from urllib.parse import urlparse
+import dns.resolver
+import ssl
+import datetime
+import os.path
+
 
 class Url:
     def __init__(self, url=""):
@@ -13,7 +23,6 @@ class Url:
 
         self.split_url()
 
-        # 101 attributes
         self.count_freq(self.url)
         self.qty_dot_url = self.get_freq('.')
         self.qty_hyphen_url = self.get_freq('-')
@@ -32,8 +41,7 @@ class Url:
         self.qty_hashtag_url = self.get_freq('#')
         self.qty_dollar_url = self.get_freq('$')
         self.qty_percent_url = self.get_freq('%')
-        self.qty_tld_url = 0
-        self.length_url = 0
+        self.length_url = self.get_length(self.url)
 
         self.count_freq(self.domain)
         self.qty_dot_domain = self.get_freq('.')
@@ -53,10 +61,7 @@ class Url:
         self.qty_hashtag_domain = self.get_freq('#')
         self.qty_dollar_domain = self.get_freq('$')
         self.qty_percent_domain = self.get_freq('%')
-        self.qty_vowels_domain = 0
-        self.domain_length = 0
-        self.domain_in_ip = 0
-        self.server_client_domain = 0
+        self.domain_length = self.get_length(self.domain)
 
         self.count_freq(self.directory)
         self.qty_dot_directory = self.get_freq('.')
@@ -76,7 +81,7 @@ class Url:
         self.qty_hashtag_directory = self.get_freq('#')
         self.qty_dollar_directory = self.get_freq('$')
         self.qty_percent_directory = self.get_freq('%')
-        self.directory_length = 0
+        self.directory_length = self.get_length(self.directory)
 
         self.count_freq(self.file)
         self.qty_dot_file = self.get_freq('.')
@@ -96,7 +101,7 @@ class Url:
         self.qty_hashtag_file = self.get_freq('#')
         self.qty_dollar_file = self.get_freq('$')
         self.qty_percent_file = self.get_freq('%')
-        self.file_length = 0
+        self.file_length = self.get_length(self.file)
 
         self.count_freq(self.params)
         self.qty_dot_params = self.get_freq('.')
@@ -116,32 +121,104 @@ class Url:
         self.qty_hashtag_params = self.get_freq('#')
         self.qty_dollar_params = self.get_freq('$')
         self.qty_percent_params = self.get_freq('%')
-        self.params_length = 0
-        self.tld_present_params = 0
-        self.qty_params = 0
+        self.params_length = self.get_length(self.params)
+        self.qty_params = self.get_number_of_params()
 
         # others
-        self.email_in_url = 0
-        self.time_response = self.get_resp_time(self.url)
-        self.domain_spf = 0
-        self.asn_ip = 0
-        self.time_domain_activation = 0
-        self.time_domain_expiration = 0
-        self.qty_ip_resolved = 0
-        self.qty_nameservers = 0
-        self.qty_mx_servers = 0
-        self.ttl_hostname = 0
-        self.tls_ssl_certificate = 0
-        self.qty_redirects = 0
-        self.url_google_index = 0
-        self.domain_google_index = 0
-        self.url_shortened = 0
+        self.response_obj = requests.get(url, allow_redirects=True)
+        self.whois_response = whois.whois(self.url)
+
+        self.time_response = self.get_response_time()
+        self.asn_ip = self.get_asn_ip()
+        self.time_domain_activation = self.get_activation_days()
+        self.time_domain_expiration = self.get_expiration_days()
+        self.qty_ip_resolved = self.get_ips_resolved()
+        self.qty_nameservers = len(self.whois_response['name_servers']) if self.whois_response['name_servers'] else -1
+        self.qty_mx_servers = self.get_mx_servers()
+        self.tls_ssl_certificate = self.has_valid_certificate()
+        self.qty_redirects = len(self.response_obj.history) - 1 if len(self.response_obj.history) > 0 else 0
+
+        self.url_shortened = 1 if len(self.url) < len(self.response_obj.url) else 0
+
+    def get_mx_servers(self):
+        mx_count = 0
+        answers = dns.resolver.query(self.domain, 'MX')
+        for _ in answers:
+            mx_count += 1
+        return mx_count
+
+    def get_asn_ip(self):
+        try:
+            c = Client()
+            ip = socket.gethostbyname(self.domain)
+            return int(c.lookup(ip).asn)
+        except Exception:
+            return  -1
+
+    def get_ips_resolved(self):
+        ip_tuple = socket.gethostbyname_ex(self.domain)
+        return len(ip_tuple) - 2
+
+    def get_response_time(self):
+        return self.response_obj.elapsed.total_seconds()
+
+    def get_activation_days(self):
+        if not self.whois_response['creation_date']:
+            return -1
+        if isinstance(self.whois_response['creation_date'], list):
+            creation_date = self.whois_response['creation_date'][0]
+        else:
+            creation_date = self.whois_response['creation_date']
+        return (datetime.datetime.today() - creation_date).days
+
+    def get_expiration_days(self):
+        if not self.whois_response['expiration_date']:
+            return -1
+        if isinstance(self.whois_response['expiration_date'], list):
+            expiration_date = self.whois_response['expiration_date'][0]
+        else:
+            expiration_date = self.whois_response['expiration_date']
+        return (expiration_date - datetime.datetime.today()).days
+
+    def ssl_expiry_datetime(self):
+        ssl_dateformat = r'%b %d %H:%M:%S %Y %Z'
+
+        context = ssl.create_default_context()
+        context.check_hostname = False
+
+        conn = context.wrap_socket(
+            socket.socket(socket.AF_INET),
+            server_hostname=self.domain,
+        )
+        # 5 second timeout
+        conn.settimeout(1.0)
+
+        conn.connect((self.domain, 443))
+        ssl_info = conn.getpeercert()
+        # Python datetime object
+        return datetime.datetime.strptime(ssl_info['notAfter'], ssl_dateformat)
+
+    def has_valid_certificate(self):
+        try:
+            now = datetime.datetime.now()
+            cert_expiry_time = self.ssl_expiry_datetime()
+            if cert_expiry_time > now:
+                return 1
+        except Exception:
+            pass
+        return 0
 
     def split_url(self):
-        self.domain = self.url.split('/')[2]
-        self.directory = self.url.split('/')[3]
-        self.file = self.url.split('/')[4].split('?')[0]
-        self.params = self.url.split('?')[-1]
+        parsed_url = urlparse(self.url)
+        self.domain = parsed_url.netloc
+        split_path = os.path.split(parsed_url.path)
+        self.params = parsed_url.query
+
+        if len(split_path) > 0:
+            self.directory = split_path[0][1:]
+
+        if len(split_path) > 1:
+            self.file = split_path[1]
 
     def count_freq(self, url):
         for i in url:
@@ -155,10 +232,19 @@ class Url:
             return self.count[symbol]
         else:
             return 0
-    
-    def get_resp_time(self, url):
-        respTime = requests.get(url)
-        return respTime.elapsed.microseconds/1000/1000
 
-url = "https://domain.com/dir/index.php?q=example"
-Url(url)
+    @staticmethod
+    def get_length(input_str):
+        return len(input_str)
+
+    def print_features(self):
+        di = {}
+        for key in self.__dict__.keys():
+            if key not in ["count", "whois_response"]:
+                di[key] = self.__dict__[key]
+        pprint(di)
+
+    def get_number_of_params(self):
+        if len(self.params) == 0:
+            return 0
+        return len(self.params.split('&'))
